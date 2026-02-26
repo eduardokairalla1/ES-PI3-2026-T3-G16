d01be63d8e03be2b86d07ac944e71b403d503439
Eduardo Kairalla <eduardokairalla123@gmail.com>
2026-02-26 09:16:53 -0300

    Add login and registration routes

--- frontend/app/lib/app/routes.dart
+++ frontend/app/lib/app/routes.dart
@@ -2,6 +2,8 @@
 
 /// --- IMPORTS ---
 import 'package:go_router/go_router.dart';
+import 'package:mesclainvest/pages/auth/login.dart';
+import 'package:mesclainvest/pages/auth/register.dart';
 import 'package:mesclainvest/pages/home/home.dart';
 
 
@@ -14,6 +16,14 @@ final router = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterPage(),
     ),
   ],
 );
