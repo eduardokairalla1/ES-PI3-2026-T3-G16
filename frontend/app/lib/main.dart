3e85f5d9f7e3c868e97a291cb897410b0d200d97
Eduardo Kairalla <eduardokairalla123@gmail.com>
2026-02-25 23:01:08 -0300

    Initialize firebase client in flutter app

--- frontend/app/lib/main.dart
+++ frontend/app/lib/main.dart
@@ -1,8 +1,10 @@
 /// --- Project entry point ---
 
 // --- IMPORTS ---
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mesclainvest/app/app.dart';
+import 'package:mesclainvest/firebase_options.dart';
 
 
 /// --- CODE ---
@@ -12,6 +14,11 @@ Future<void> main() async {
 
   // ensure Flutter engine is initialized before async calls
   WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize firebase client
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // start root widget
   runApp(const MesclaInvestApp());
