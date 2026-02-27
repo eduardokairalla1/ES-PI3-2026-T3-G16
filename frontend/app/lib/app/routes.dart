234c0bf5cbef8cf7ce32756689562883665218d9
Eduardo Kairalla <eduardokairalla123@gmail.com>
2026-02-27 19:48:28 -0300

    Add redirect logic for dashboard access based on authentication status

--- frontend/app/lib/app/routes.dart
+++ frontend/app/lib/app/routes.dart
@@ -1,9 +1,11 @@
 /// --- App routes ---
 
 /// --- IMPORTS ---
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mesclainvest/pages/auth/login.dart';
 import 'package:mesclainvest/pages/auth/register.dart';
+import 'package:mesclainvest/pages/dashboard/dashboard.dart';
 import 'package:mesclainvest/pages/home/home.dart';
 
 
@@ -12,6 +14,22 @@ import 'package:mesclainvest/pages/home/home.dart';
 // initialize app router
 final router = GoRouter(
   initialLocation: '/',
+  redirect: (context, state) {
+
+    // check if user is logged
+    final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+
+    // check if user is trying to access the dashboard
+    final isDashboard = state.matchedLocation == '/dashboard';
+
+    // user is not logged and trying to access the dashboard: redirect to login
+    if (!isLoggedIn && isDashboard) return '/login';
+
+    // user is logged and trying to access login or register: redirect to
+    // dashboard
+    return null;
+
+  },
   routes: [
     GoRoute(
       path: '/',
@@ -24,6 +42,10 @@ final router = GoRouter(
     GoRoute(
       path: '/register',
       builder: (context, state) => const RegisterPage(),
+    ),
+    GoRoute(
+      path: '/dashboard',
+      builder: (context, state) => const DashboardPage(),
     ),
   ],
 );
