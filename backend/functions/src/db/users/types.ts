5a5da92d914164080ea2ab23d35e042f27b5fc52
Davi Shieh <davishieh@users.noreply.github.com>
2026-03-04 00:58:51 -0300

    Add User schema and type

---
+++ backend/functions/src/db/users/types.ts
@@ -0,0 +1,27 @@
+/**
+ * User types and schema.
+ *
+ * Davi da Cruz Shieh - 24798076
+ */
+
+/**
+ * IMPORTS
+ */
+import * as z from 'zod';
+
+
+/**
+ * TYPES
+ */
+export const userSchema = z.object(
+    {
+        created_at: z.iso.datetime(),
+        email: z.email(),
+        full_name: z.string(),
+        status: z.enum(['active', 'inactive']),
+        uid: z.string(),
+        updated_at:  z.nullable(z.iso.datetime()),
+    },
+);
+
+export type User = z.infer<typeof userSchema>;
