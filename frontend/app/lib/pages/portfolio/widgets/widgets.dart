aa0a40aff8d820e7a61ac28aaac89f6373f5c8bb
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-04-23 15:44:03 -0300

    Adição de novo widget

--- frontend/app/lib/pages/portfolio/widgets/widgets.dart
+++ frontend/app/lib/pages/portfolio/widgets/widgets.dart
@@ -5,3 +5,5 @@ export 'cartao_patrimonio_portfolio.dart';
 export 'grafico_evolucao.dart';
 export 'distribuicao_patrimonio.dart';
 export 'historico_transacoes.dart';
+export 'portfolio_styles.dart';
+export 'cartao_base.dart';
