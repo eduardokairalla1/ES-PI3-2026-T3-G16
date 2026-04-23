383ef9110f22a16aad39edd54d31c9d0aab4502a
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-04-23 15:35:30 -0300

    Widget da página

---
+++ frontend/app/lib/pages/portfolio/widgets/widgets.dart
@@ -0,0 +1,7 @@
+/// --- Widgets Barrel File ---
+
+export 'cabecalho_portfolio.dart';
+export 'cartao_patrimonio_portfolio.dart';
+export 'grafico_evolucao.dart';
+export 'distribuicao_patrimonio.dart';
+export 'historico_transacoes.dart';
