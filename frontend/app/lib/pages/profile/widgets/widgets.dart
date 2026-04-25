41badb34c59c5c790f5c937cee7aff9c3a68161d
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-04-25 11:33:13 -0300

    Ideia de widget inicial

---
+++ frontend/app/lib/pages/profile/widgets/widgets.dart
@@ -0,0 +1,8 @@
+/// Barrel file para exportação unificada de widgets do perfil.
+
+export 'cabecalho_perfil.dart';
+export 'info_usuario.dart';
+export 'cartao_estatisticas.dart';
+export 'tile_acao_perfil.dart';
+export 'botao_sair.dart';
+export 'navegacao_inferior_mock.dart';
