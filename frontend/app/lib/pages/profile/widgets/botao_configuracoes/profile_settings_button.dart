ece6585a363f635a7dc3ace15f07c45d8cf91f16
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-04-28 18:18:54 -0300

    botão de config

---
+++ frontend/app/lib/pages/profile/widgets/botao_configuracoes/profile_settings_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mesclainvest/pages/profile/widgets/profile_action_tile.dart';
+
+class ProfileSettingsButton extends StatelessWidget {
+  const ProfileSettingsButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProfileActionTile(
+      icon: Icons.settings_outlined,
+      title: 'Configuracoes',
+      onTap: () => context.push('/settings'),
+    );
+  }
+}
