fe513291060929dc651d9d5712b00805211318be
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-04-28 18:15:42 -0300

    pontos que compõem config

---
+++ frontend/app/lib/pages/profile/widgets/botao_configuracoes/settings_group.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SettingsGroup extends StatelessWidget {
+  final List<Widget> children;
+
+  const SettingsGroup({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: const Color(0xFFEBEBEB)),
+      ),
+      child: Column(
+        children: children,
+      ),
+    );
+  }
+}
+
+class SettingsDivider extends StatelessWidget {
+  const SettingsDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 56),
+      height: 1,
+      color: const Color(0xFFF2F2F2),
+    );
+  }
+}
