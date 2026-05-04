5c1080ac149b2edf5e17b6532df0e3c0318af76b
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-05-04 08:53:06 -0300

    Finalizando Dashboard Backend

---
+++ backend/functions/src/db/favorites/model.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Favorite types and schema.
+ *
+ * Alex Gabriel Soares Sousa - 24802449
+ */
+
+
+/**
+ * TYPES
+ */
+
+export interface FavoriteDocument
+{
+    created_at: Date;
+    startup_id: string;
+}
