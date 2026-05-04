6372e32820e354ae1060c206765f65681108ebb4
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-05-04 08:54:07 -0300

    Update fix dashboard

---
+++ backend/functions/src/db/investments/storage.ts
@@ -0,0 +1,40 @@
+/**
+ * Investment database operations.
+ *
+ * Alex Gabriel Soares Sousa - 24802449
+ */
+
+/**
+ * IMPORTS
+ */
+import db from '../../configs';
+
+
+/**
+ * TYPES
+ */
+import type {InvestmentDocument} from './model';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I get all investments for a user.
+ *
+ * @param uid Firebase Auth UID of the user
+ *
+ * @returns list of investment documents
+ */
+export async function getUserInvestments(uid: string): Promise<InvestmentDocument[]>
+{
+    const snapshot = await db
+        .collection('users')
+        .doc(uid)
+        .collection('investments')
+        .orderBy('startup_name')
+        .get();
+
+    return snapshot.docs.map(doc => ({id: doc.id, ...doc.data()} as InvestmentDocument));
+}
