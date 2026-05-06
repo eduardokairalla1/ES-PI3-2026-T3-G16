34420d58ca3314650fa0d361afe139282dc40a75
davi.shieh <davishieh@gmail.com>
2026-05-06 00:23:00 -0300

    Add ShimmerBox widget with animated gradient

---
+++ frontend/app/lib/shared/widgets/shimmer_box.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class ShimmerBox extends StatefulWidget {
+
+  final double width;
+  final double height;
+  final BorderRadius borderRadius;
+
+  const ShimmerBox({
+    super.key,
+    required this.width,
+    required this.height,
+    this.borderRadius = const BorderRadius.all(Radius.circular(8)),
+  });
+
+  @override
+  State<ShimmerBox> createState() => _ShimmerBoxState();
+}
+
+class _ShimmerBoxState extends State<ShimmerBox> with SingleTickerProviderStateMixin {
+
+  late final AnimationController _ctrl;
+  late final Animation<double> _anim;
+
+  @override
+  void initState() {
+    super.initState();
+    _ctrl = AnimationController(vsync: this, duration: const Duration(milliseconds: 1200))
+      ..repeat();
+    _anim = Tween<double>(begin: -1.5, end: 2.5).animate(
+      CurvedAnimation(parent: _ctrl, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _anim,
+      builder: (_, _) {
+        return Container(
+          width: widget.width,
+          height: widget.height,
+          decoration: BoxDecoration(
+            borderRadius: widget.borderRadius,
+            gradient: LinearGradient(
+              begin: Alignment(_anim.value - 1, 0),
+              end:   Alignment(_anim.value,     0),
+              colors: const [
+                Color(0xFFEEEEEE),
+                Color(0xFFF8F8F8),
+                Color(0xFFEEEEEE),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
