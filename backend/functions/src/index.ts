0ccc9c904faaf7ee61f724ecc900915d94dae08f
davi.shieh <davishieh@gmail.com>
2026-05-06 23:13:11 -0300

    Register order and portfolio callables in backend

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -13,5 +13,5 @@ import './configs';
 /**
  * --- EXPORTS ---
  */
-export {onUserCreated, onGetProfile, onUpdateProfile, onToggle2FA} from './responders/index';
-export {onGetStartups, onGetStartup, onSendQuestion, onGetQuestions} from './responders/index';
+export {onUserCreated, onGetProfile, onGetWallet, onCreateOrder, onGetPortfolio, onUpdateProfile, onToggle2FA} from './responders/index';
+export {onGetStartups, onGetStartup, onSendQuestion, onGetQuestions, onGetTokenHistory} from './responders/index';
