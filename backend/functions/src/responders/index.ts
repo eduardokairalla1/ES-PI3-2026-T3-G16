1027997aa0694f62b576eb54d5537bc6e53593c1
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-05-09 21:04:35 -0300

    Respond correção

--- backend/functions/src/responders/index.ts
+++ backend/functions/src/responders/index.ts
@@ -23,13 +23,13 @@ import {handleOnSendQuestion} from '../services/startup/onSendQuestion';
  */
 
 // user functions
-export const onUserCreated    = onCall(handleOnUserCreated);
-export const onGetProfile     = onCall(handleOnGetProfile);
-export const onUpdateProfile  = onCall(handleOnUpdateProfile);
-export const onToggle2FA      = onCall(handleOnToggle2FA);
+export const onUserCreated    = onCall({cors: true}, handleOnUserCreated);
+export const onGetProfile     = onCall({cors: true}, handleOnGetProfile);
+export const onUpdateProfile  = onCall({cors: true}, handleOnUpdateProfile);
+export const onToggle2FA      = onCall({cors: true}, handleOnToggle2FA);
 
 // startup functions
-export const onGetStartups   = onCall(handleOnGetStartups);
-export const onGetStartup    = onCall(handleOnGetStartup);
-export const onSendQuestion  = onCall(handleOnSendQuestion);
-export const onGetQuestions  = onCall(handleOnGetQuestions);
+export const onGetStartups   = onCall({cors: true}, handleOnGetStartups);
+export const onGetStartup    = onCall({cors: true}, handleOnGetStartup);
+export const onSendQuestion  = onCall({cors: true}, handleOnSendQuestion);
+export const onGetQuestions  = onCall({cors: true}, handleOnGetQuestions);
