55aaeb56fc7d59a7ef0180d1b3bd011e0205e8c4
Eduardo Kairalla <eduardokairalla123@gmail.com>
2026-05-26 23:38:53 -0300

    Wire 2FA callables into responders and replace onToggle2FA

--- backend/functions/src/responders/index.ts
+++ backend/functions/src/responders/index.ts
@@ -15,12 +15,14 @@ import {handleOnGetMyOrders} from '../services/user/onGetMyOrders';
 import {handleOnGetPortfolio} from '../services/user/onGetPortfolio';
 import {handleOnGetProfile} from '../services/user/onGetProfile';
 import {handleOnGetWallet} from '../services/user/onGetWallet';
-import {handleOnToggle2FA} from '../services/user/onToggle2FA';
+import {handleOnConfirmSetup2FA} from '../services/user/onConfirmSetup2FA';
+import {handleOnDisable2FA} from '../services/user/onDisable2FA';
+import {handleOnSetup2FA} from '../services/user/onSetup2FA';
 import {handleOnUpdateProfile} from '../services/user/onUpdateProfile';
 import {handleOnUserCreated} from '../services/user/onUserCreated';
 import {handleOnDeposit} from '../services/user/onDeposit';
 import {handleOnGetTransactions} from '../services/user/onGetTransactions';
-
+import {handleOnVerify2FA} from '../services/user/onVerify2FA';
 import {handleOnGetQuestions} from '../services/startup/onGetQuestions';
 import {handleOnGetStartup} from '../services/startup/onGetStartup';
 import {handleOnGetStartups} from '../services/startup/onGetStartups';
@@ -37,18 +39,21 @@ import {handleOnGetPatrimonyHistory} from '../services/dashboard/onGetPatrimonyH
  */
 
 // user functions
-export const onUserCreated     = onCall({cors: true}, handleOnUserCreated);
-export const onGetProfile      = onCall({cors: true}, handleOnGetProfile);
-export const onGetWallet       = onCall({cors: true}, handleOnGetWallet);
-export const onCreateOrder     = onCall({cors: true}, handleOnCreateOrder);
-export const onBuyFromStartup  = onCall({cors: true}, handleOnBuyFromStartup);
-export const onCancelOrder     = onCall({cors: true}, handleOnCancelOrder);
-export const onGetMyOrders     = onCall({cors: true}, handleOnGetMyOrders);
-export const onGetPortfolio    = onCall({cors: true}, handleOnGetPortfolio);
-export const onUpdateProfile   = onCall({cors: true}, handleOnUpdateProfile);
-export const onToggle2FA       = onCall({cors: true}, handleOnToggle2FA);
-export const onDeposit         = onCall({cors: true}, handleOnDeposit);
-export const onGetTransactions = onCall({cors: true}, handleOnGetTransactions);
+export const onUserCreated      = onCall({cors: true}, handleOnUserCreated);
+export const onGetProfile       = onCall({cors: true}, handleOnGetProfile);
+export const onGetWallet        = onCall({cors: true}, handleOnGetWallet);
+export const onCreateOrder      = onCall({cors: true}, handleOnCreateOrder);
+export const onBuyFromStartup   = onCall({cors: true}, handleOnBuyFromStartup);
+export const onCancelOrder      = onCall({cors: true}, handleOnCancelOrder);
+export const onGetMyOrders      = onCall({cors: true}, handleOnGetMyOrders);
+export const onGetPortfolio     = onCall({cors: true}, handleOnGetPortfolio);
+export const onUpdateProfile    = onCall({cors: true}, handleOnUpdateProfile);
+export const onSetup2FA         = onCall({cors: true}, handleOnSetup2FA);
+export const onConfirmSetup2FA  = onCall({cors: true}, handleOnConfirmSetup2FA);
+export const onVerify2FA        = onCall({cors: true}, handleOnVerify2FA);
+export const onDisable2FA       = onCall({cors: true}, handleOnDisable2FA);
+export const onDeposit          = onCall({cors: true}, handleOnDeposit);
+export const onGetTransactions  = onCall({cors: true}, handleOnGetTransactions);
 
 
 // startup functions
@@ -58,6 +63,7 @@ export const onGetStartupOrderBook = onCall({cors: true}, handleOnGetStartupOrde
 export const onSendQuestion        = onCall({cors: true}, handleOnSendQuestion);
 export const onGetQuestions        = onCall({cors: true}, handleOnGetQuestions);
 export const onGetTokenHistory     = onCall({cors: true}, handleOnGetTokenHistory);
+
 
 // dashboard functions
 export const onGetDashboard        = onCall({cors: true}, handleOnGetDashboard);
